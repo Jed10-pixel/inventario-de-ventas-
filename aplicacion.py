040f9cd55ddd5915f420daddfbf2f72f481bf296
Jed10-pixel <duquejuanestebam@gmail.com>
2026-06-21 15:07:27 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -45,4 +45,7 @@ def index():
     return render_template('index.html', ventas=lista_ventas, error=error)
 
 if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    aplicacion.run(host='0.0.0.0', port=port)
     app.run(debug=True)
